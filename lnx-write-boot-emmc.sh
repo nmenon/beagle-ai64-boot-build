4487c0c8f6b973f6c016c4d9eeb1b056734ce68b
Nishanth Menon <nm@ti.com>
2022-03-04 09:29:43 -0600

    lnx-write-boot-emmc.sh: Update to the latest sequence
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- lnx-write-boot-emmc.sh
+++ lnx-write-boot-emmc.sh
@@ -13,8 +13,23 @@ if [ -d ${wdir} ] ; then
 	mmc hwreset enable /dev/mmcblk0
 	set -e
 	echo '0' >> /sys/class/block/mmcblk0boot0/force_ro
-	dd if=${wdir}/tiboot3.bin of=/dev/mmcblk0boot0 count=3 bs=128k
-	dd if=${wdir}/tispl.bin of=/dev/mmcblk0boot0 seek=3 count=6 bs=128k
-	dd if=${wdir}/u-boot.img of=/dev/mmcblk0boot0 seek=9 count=16 bs=128k
-	dd if=${wdir}/sysfw.itb of=/dev/mmcblk0boot0 seek=25 count=3 bs=128k
+
+	# Nuke the contents of emmc boot0 first
+	echo "dd if=/dev/zero of=/dev/mmcblk0boot0 count=32 bs=128k"
+	dd if=/dev/zero of=/dev/mmcblk0boot0 count=32 bs=128k
+
+	# Put the tiboot3.bin in mmcblk0boot0
+	dd if=${wdir}/tiboot3.bin of=/dev/mmcblk0boot0 bs=128k
+
+	# Now the rest of the images in the UDA boot partition
+	boot_partition=`mktemp -d`
+	mount /dev/mmcblk0p1 ${boot_partition}
+	cp -v ${wdir}/tispl.bin \
+		${wdir}/u-boot.img \
+		${wdir}/sysfw.itb \
+		${boot_partition}
+	umount ${boot_partition}
+	sync
+	sync
+	rm -rvf ${boot_partition}
 fi
